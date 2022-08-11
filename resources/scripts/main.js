c0b6cde289ef67675a169bcb270d615b5be81328
shawiizz <shaweereso@gmail.com>
2022-08-11 21:36:18 +0200

    language system

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,5 +1,50 @@
 const titleElement = document.getElementById('username');
 const arrowHeadElement = document.getElementById('arrow-head');
+const dropBtnElement = document.getElementsByClassName('dropbtn')[0]
+let currentLang = 'en'
+
+const savedLangs = new Map();
+
+function parseLang(id) {
+    if(savedLangs.has(id)) return savedLangs.get(id)
+
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", location.origin+'/resources/lang/'+id+'.txt', false ); // false for synchronous request
+    xmlHttp.send( null );
+    const langMap = new Map()
+    for(const line of xmlHttp.responseText.split('\n')) {
+        const key = line.split('=')[0]
+        langMap.set(key, line.replace(key+'=', ''))
+    }
+    savedLangs.set(id, langMap)
+    return langMap;
+}
+
+for(const buttonLang of document.getElementsByClassName('dropdown-button')) {
+    buttonLang.addEventListener('click', () => {
+        currentLang = buttonLang.id;
+        setLang(buttonLang.id);
+
+        const oldId = buttonLang.id;
+        const oldText = buttonLang.innerText;
+        buttonLang.id = dropBtnElement.id;
+        buttonLang.innerText = dropBtnElement.innerText;
+        dropBtnElement.id = oldId
+        dropBtnElement.innerText = oldText;
+    })
+}
+
+function setLang(id) {
+    const parsedLang = parseLang(id)
+
+    for(const langElement of document.querySelectorAll("[keylang]")) {
+        if(!parsedLang.has(langElement.getAttribute('keylang'))) {
+            console.log('Missing keylang: '+langElement.getAttribute('keylang')+' in '+id+' lang.');
+        }
+        console.log(parsedLang.get(langElement.getAttribute('keylang')))
+        langElement.innerHTML = parsedLang.get(langElement.getAttribute('keylang'))
+    }
+}
 
 arrowHeadElement.addEventListener('click', () => {
     document.getElementById('myself').scrollIntoView();
